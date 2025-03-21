1b08a2df3f94a1af58e624a0f3f207704581b4ff
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:26:45 +0930

    Update ogr_vector.py

--- ogr_vector.py
+++ ogr_vector.py
@@ -1,6 +1,6 @@
 from osgeo import ogr
 
-district_path = 'C:\\Users\\qw2\\Desktop\\Feed_Outlook_Data\\Pastoral_districts\\NT_pastoral_districts_WGS84.gpkg'
+district_path = 'Feed_Outlook_Data\\Pastoral_districts\\NT_pastoral_districts_WGS84.gpkg'
 
 def loop_zonal_stats(input_zone_polygon):
     shp = ogr.Open(input_zone_polygon)
