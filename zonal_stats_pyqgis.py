65662b7cbbd17f2c8fb734c6a1c34fd4bc68de0e
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:29:15 +0930

    Update zonal_stats_pyqgis.py

--- zonal_stats_pyqgis.py
+++ zonal_stats_pyqgis.py
@@ -1,5 +1,5 @@
-zonal_stats_params = {'INPUT':'C:/Users/qw2/Desktop/Feed_Outlook_Data/Pastoral_districts/NT_pastoral_districts_WGS84.gpkg|layername=NT_pastoral_districts_WGS84',
-                        'INPUT_RASTER':'C:/Users/qw2/AppData/Local/Temp/processing_EQkBVS/5a5185346e7842b999d1fafa59027709/OUTPUT.tif',
+zonal_stats_params = {'INPUT':'Feed_Outlook_Data/Pastoral_districts/NT_pastoral_districts_WGS84.gpkg|layername=NT_pastoral_districts_WGS84',
+                        'INPUT_RASTER':'AppData/Local/Temp/processing_EQkBVS/5a5185346e7842b999d1fafa59027709/OUTPUT.tif',
                         'RASTER_BAND':1,
                         'COLUMN_PREFIX':'_',
                         'STATISTICS':[2,3],
